acd733e4ad52c64187624ee094596c65deb191d3
nazrinqurbanova <162806647+nazrinqurbanova@users.noreply.github.com>
2025-10-08 07:44:09 +0400

    Add project files.

---
+++ ticketSystems/Form1.Designer.cs
@@ -0,0 +1,446 @@
+﻿namespace ticketSystems
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox4 = new TextBox();
+            maskedTextBox3 = new MaskedTextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label10 = new Label();
+            label9 = new Label();
+            label7 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            groupBox2 = new GroupBox();
+            label8 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            groupBox1 = new GroupBox();
+            label5 = new Label();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            panel2 = new Panel();
+            button4 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(151, 227);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(156, 27);
+            textBox4.TabIndex = 10;
+            // 
+            // maskedTextBox3
+            // 
+            maskedTextBox3.Location = new Point(151, 175);
+            maskedTextBox3.Margin = new Padding(3, 4, 3, 4);
+            maskedTextBox3.Mask = "(+994) 00-000-00-00";
+            maskedTextBox3.Name = "maskedTextBox3";
+            maskedTextBox3.Size = new Size(156, 27);
+            maskedTextBox3.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(151, 115);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(156, 27);
+            textBox3.TabIndex = 6;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(151, 44);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(156, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 0, 0);
+            button1.Location = new Point(184, 279);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 40);
+            button1.TabIndex = 4;
+            button1.Text = "Bilet al";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.ButtonShadow;
+            label10.Location = new Point(79, 228);
+            label10.Name = "label10";
+            label10.Size = new Size(49, 20);
+            label10.TabIndex = 3;
+            label10.Text = "Email:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = SystemColors.ButtonShadow;
+            label9.Location = new Point(69, 176);
+            label9.Name = "label9";
+            label9.Size = new Size(61, 20);
+            label9.TabIndex = 2;
+            label9.Text = "Telefon:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ButtonShadow;
+            label7.Location = new Point(39, 56);
+            label7.Name = "label7";
+            label7.Size = new Size(93, 20);
+            label7.TabIndex = 0;
+            label7.Text = "Ad və soyad:";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Maroon;
+            button2.Location = new Point(302, 37);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(32, 97);
+            button2.TabIndex = 10;
+            button2.Text = "<\r\n>\r\n";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(130, 279);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(130, 227);
+            maskedTextBox2.Margin = new Padding(3, 4, 3, 4);
+            maskedTextBox2.Mask = "00:00";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(183, 27);
+            maskedTextBox2.TabIndex = 8;
+            maskedTextBox2.ValidatingType = typeof(DateTime);
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(130, 171);
+            maskedTextBox1.Margin = new Padding(3, 4, 3, 4);
+            maskedTextBox1.Mask = "00/00/0000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(183, 27);
+            maskedTextBox1.TabIndex = 7;
+            maskedTextBox1.ValidatingType = typeof(DateTime);
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Bakı", "İstanbul", "Tehran", "Praqa", "Pekin" });
+            comboBox2.Location = new Point(130, 104);
+            comboBox2.Margin = new Padding(3, 4, 3, 4);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(162, 28);
+            comboBox2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Bakı", "İstanbul", "Tehran", "Praqa", "Pekin" });
+            comboBox1.Location = new Point(130, 37);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(162, 28);
+            comboBox1.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.AppWorkspace;
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(maskedTextBox3);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label7);
+            groupBox2.ForeColor = Color.White;
+            groupBox2.Location = new Point(454, 139);
+            groupBox2.Margin = new Padding(3, 4, 3, 4);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(3, 4, 3, 4);
+            groupBox2.Size = new Size(328, 331);
+            groupBox2.TabIndex = 7;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Sərnişin məlumatları";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = SystemColors.ButtonShadow;
+            label8.Location = new Point(81, 116);
+            label8.Name = "label8";
+            label8.Size = new Size(34, 20);
+            label8.TabIndex = 1;
+            label8.Text = "FİN:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ButtonShadow;
+            label6.Location = new Point(49, 284);
+            label6.Name = "label6";
+            label6.Size = new Size(32, 20);
+            label6.TabIndex = 4;
+            label6.Text = "Yer:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ButtonShadow;
+            label4.Location = new Point(47, 176);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Tarix:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ButtonShadow;
+            label3.Location = new Point(47, 105);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Haraya:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ButtonShadow;
+            label2.Location = new Point(47, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Haradan:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.AppWorkspace;
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(maskedTextBox2);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Location = new Point(49, 139);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(357, 331);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Tag = "";
+            groupBox1.Text = " Marşurut";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ButtonShadow;
+            label5.Location = new Point(49, 228);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Saat:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(49, 16);
+            listBox1.Margin = new Padding(3, 4, 3, 4);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(732, 104);
+            listBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkTurquoise;
+            button3.Location = new Point(49, 139);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(177, 43);
+            button3.TabIndex = 0;
+            button3.Text = "Siyahıdan sil";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Teal;
+            panel2.Controls.Add(listBox1);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 502);
+            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(968, 197);
+            panel2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.DarkTurquoise;
+            button4.Location = new Point(605, 139);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(177, 43);
+            button4.TabIndex = 1;
+            button4.Text = "Proqramdan çıxış";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(351, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(281, 43);
+            label1.TabIndex = 1;
+            label1.Text = "TRAVEL TİCKET";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(49, 16);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(245, 115);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Teal;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(968, 135);
+            panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(968, 699);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox textBox4;
+        private MaskedTextBox maskedTextBox3;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label10;
+        private Label label9;
+        private Label label7;
+        private Button button2;
+        private TextBox textBox1;
+        private MaskedTextBox maskedTextBox2;
+        private MaskedTextBox maskedTextBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private GroupBox groupBox2;
+        private Label label8;
+        private Label label6;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private GroupBox groupBox1;
+        private Label label5;
+        private ListBox listBox1;
+        private Button button3;
+        private Panel panel2;
+        private Button button4;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+    }
+}
---
+++ ticketSystems/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Windows.Forms; // Added to ensure Form and MessageBox are recognized
+
+namespace ticketSystems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string haradan = comboBox1.Text;
+            string haraya = comboBox2.Text;
+            string tarix = maskedTextBox1.Text;
+            string saat = maskedTextBox2.Text;
+            string yer = textBox1.Text;
+            string adSoyad = textBox2.Text;
+            string fin = textBox3.Text;
+            string telefon = maskedTextBox3.Text;
+            string email = textBox4.Text;
+
+            if (string.IsNullOrWhiteSpace(adSoyad) ||
+                string.IsNullOrWhiteSpace(fin) ||
+                string.IsNullOrWhiteSpace(telefon) ||
+                string.IsNullOrWhiteSpace(email) ||
+                comboBox1.SelectedIndex == -1 ||
+                comboBox2.SelectedIndex == -1 ||
+                string.IsNullOrWhiteSpace(tarix) ||
+                string.IsNullOrWhiteSpace(saat) ||
+                string.IsNullOrWhiteSpace(yer))
+            {
+                MessageBox.Show("Zəhmət olmasa bütün xanaları doldurun!", "Xəta");
+                return;
+            }
+            if (!FinYoxla(fin) || !EmailYoxla(email))
+            {
+                return;
+            }
+
+            string info = $"Marsrut: {haradan} → {haraya} | Tarix: {tarix} | Saat: {saat} | " +
+                            $"Yer: {yer} | Sərnisin: {adSoyad} | FİN: {fin} | Tel: {telefon} | Email: {email}";
+            listBox1.Items.Add(info);
+
+            MessageBox.Show("Bilet uğurla əlavə olundu!", "Məlumat");
+
+            textBox2.Clear();
+            textBox3.Clear();
+            maskedTextBox3.Clear();
+            textBox4.Clear();
+            maskedTextBox1.Clear();
+            maskedTextBox2.Clear();
+            textBox1.Clear();
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+        } 
+
+        private bool FinYoxla(string fin)
+        {
+            if (fin.Length != 7)
+            {
+                MessageBox.Show("FİN 7 simvoldan ibarət olmalıdır!", "Xəta");
+                return false;
+            }
+            foreach (char c in fin)
+            {
+                if (!char.IsUpper(c) && !char.IsDigit(c))
+                {
+                    MessageBox.Show("FİN yalnız böyük hərf və rəqəmlərdən ibarət ola bilər!", "Xəta");
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool EmailYoxla(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (!Regex.IsMatch(email, pattern))
+            {
+                MessageBox.Show("Email düzgün formatda deyil! (nümunə: example@mail.com)", "Xəta");
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                MessageBox.Show("Seçilmiş bilet siyahıdan silindi.", "Məlumat");
+            }
+            else
+            {
+                MessageBox.Show("Zəhmət olmasa silmək üçün siyahıdan bilet seçin!", "Xəta");
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(comboBox1.Text) &&
+                !string.IsNullOrWhiteSpace(comboBox2.Text))
+            {
+                string temp = comboBox1.Text;
+                comboBox1.Text = comboBox2.Text;
+                comboBox2.Text = temp;
+            }
+            else
+            {
+                MessageBox.Show("Zəhmət olmasa hər iki məntəqəni seçin!", "Xəta");
+            }
+        }
+    }
+}
